c4d3bfd631d41f0f74131901f5305e33f0336e7c
arunkotla <arunkkotla1998@gmail.com>
2023-12-29 00:25:16 +0530

    Modular PriceFeed incorporation

--- Contracts/FundMe.sol
+++ Contracts/FundMe.sol
@@ -13,42 +13,47 @@ contract FundMe {
     address[] public funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public immutable  i_owner;
+    address public immutable i_owner;
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
-    
-    constructor() {
+
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address priceFeedaddress) {
         i_owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedaddress);
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        require(
+            msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
-    
-    function getVersion() public view returns (uint256){
-        // ETH/USD price feed address of Sepolia Network.
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
-    
-    modifier onlyOwner {
+
+    modifier onlyOwner() {
         // require(msg.sender == owner);
         if (msg.sender != i_owner) revert NotOwner();
         _;
     }
-    
+
     function withdraw() public onlyOwner {
-        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(callSuccess, "Call failed");
     }
-
 
     fallback() external payable {
         fund();
@@ -57,5 +62,4 @@ contract FundMe {
     receive() external payable {
         fund();
     }
-
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+
+  networks: {
+    ropsten: {
+      url: "",
+      accounts: [],
+      chainID: "",
+    },
+  },
+
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      4: 1,
+    },
+  },
 };
